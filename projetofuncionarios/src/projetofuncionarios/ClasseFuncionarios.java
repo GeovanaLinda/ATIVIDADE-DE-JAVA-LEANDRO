3f4fbeed56314a857e612e7f8b567f1154dbfb0d
GeovanaLinda <104873335+GeovanaLinda@users.noreply.github.com>
2022-11-25 22:46:00 -0300

    Add files via upload

---
+++ projetofuncionarios/src/projetofuncionarios/ClasseFuncionarios.java
@@ -0,0 +1,36 @@
+package projetofuncionarios;
+
+public class ClasseFuncionarios {
+
+	private int atributoIdFuncionario; 
+	private String atributoNomeFuncionario; 
+	public int getAtributoIdFuncionario() {
+		return atributoIdFuncionario;
+	}
+
+	public void setAtributoIdFuncionario(int atributoIdFuncionario) {
+		this.atributoIdFuncionario = atributoIdFuncionario;
+	}
+
+	public String getAtributoNomeFuncionario() {
+		return atributoNomeFuncionario;
+	}
+
+	public void setAtributoNomeFuncionario(String atributoNomeFuncionario) {
+		this.atributoNomeFuncionario = atributoNomeFuncionario;
+	}
+
+	public double getAtributoRendaFuncionario() {
+		return atributoRendaFuncionario;
+	}
+
+	public void setAtributoRendaFuncionario(double atributoRendaFuncionario) {
+		this.atributoRendaFuncionario = atributoRendaFuncionario;
+	}
+
+	private double atributoRendaFuncionario;
+
+	
+
+
+}
---
+++ projetofuncionarios/src/projetofuncionarios/ClasseInsertSQL.java
@@ -0,0 +1,45 @@
+package projetofuncionarios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ClasseInsertSQL {
+
+	public void salvarDados(ClasseFuncionarios funcionario) {
+		
+	String insert_sql = "INSERT INTO tabelafuncionarios VALUES(default, ?, ?)";
+	Connection acesso = null; // import java.sql
+	PreparedStatement preparar = null; // import java.sql
+	
+	try {
+		acesso = ClasseDadosConexao.criarConexao();//Juntar dados do banco com o comando sql insert
+		preparar = acesso.prepareStatement(insert_sql);
+		preparar.setString(1, funcionario.getAtributoNomeFuncionario());
+		preparar.setDouble(2, funcionario.getAtributoRendaFuncionario());
+		preparar.execute();
+		}
+		catch(Exception e) {
+		System.out.println("Erro ao acessar o banco!" + e);
+		}
+	finally {
+		try {
+		if(preparar != null) {
+		preparar.close();
+		}
+		if(acesso != null) {
+		acesso.close();
+		}
+		}catch(Exception e) {
+		System.out.println();
+		}
+		}
+
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
